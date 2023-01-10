6654b1fd7267d2c77d7749951826976cdaf53e71
ItWasSeer <casualC-er@users.noreply.github.com>
2023-01-10 18:05:47 +0200

    added fun page

---
+++ pages/fun.vue
@@ -0,0 +1,6 @@
+<template>
+    <NuxtLink to="/">Home page</NuxtLink>
+    <NuxtLink to="/fun">This Page!</NuxtLink>
+    <a href="https://github.com/CasualC-er/FreeMessage-client">Source Code</a>
+
+</template>
