c05eb5bc82a40c6d929ad8843fb85b8de5cb3567
CasualC-er <88845943+CasualC-er@users.noreply.github.com>
2023-01-26 11:02:40 +0200

    new file:   components/Login.vue 	deleted:    components/header.vue 	deleted:    components/login.vue 	modified:   pages/fun.vue 	modified:   pages/index.vue

--- pages/fun.vue
+++ pages/fun.vue
@@ -1,16 +1,17 @@
 <template>
+    <Header />
     <div class="mid">
         <NuxtLink to="/">Home page</NuxtLink>
     </div>
-        <NuxtLink to="/fun">This Page!</NuxtLink>
+    <NuxtLink to="/fun">This Page!</NuxtLink>
     <div>
         <a href="https://github.com/CasualC-er/FreeMessage-client">Source Code</a>
     </div>
 </template>
 
 <style>
-    .mid{
-        display: flex;
-        justify-content: center;
-    }
+.mid {
+    display: flex;
+    justify-content: center;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-    <h5>d</h5>
+    <h1 class="Head">Wellcome to the FreeMessage client!</h1>
+    <NuxtLink to="/fun">Fun Page</NuxtLink>
 </template>
+
+<style>
+    .Head {
+        text-align: center;
+        font-size: xx-large;
+        font-weight: 100;
+}</style>
