e40f61d4dd71cd26b44e9e2724a61512d31a288c
CasualC-er <88845943+CasualC-er@users.noreply.github.com>
2023-02-03 10:30:05 +0200

    add buttons and headers

--- pages/fun.vue
+++ pages/fun.vue
@@ -1,17 +1,7 @@
 <template>
     <Header />
-    <div class="mid">
-        <NuxtLink to="/">Home page</NuxtLink>
-    </div>
     <NuxtLink to="/fun">This Page!</NuxtLink>
     <div>
         <a href="https://github.com/CasualC-er/FreeMessage-client">Source Code</a>
     </div>
 </template>
-
-<style>
-.mid {
-    display: flex;
-    justify-content: center;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,8 @@
 <template>
-    <h1 class="Head">Wellcome to the FreeMessage client!</h1>
+    <h1 class="Header">Wellcome to the FreeMessage client!</h1>
     <NuxtLink to="/fun">Fun Page</NuxtLink>
+    <div class="text-center">
+        <h1 class="text-3xl to-blue-500 ">gfdgdf</h1>
+    </div>
+    <Counter />
 </template>
-
-<style>
-    .Head {
-        text-align: center;
-        font-size: xx-large;
-        font-weight: 100;
-}</style>
